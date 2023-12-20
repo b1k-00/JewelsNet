f81705c5b82a689731f8bc680ed8b78a9224b97d
BC0LL1N5 <brennencollins44@gmail.com>
2023-12-20 14:41:38 -0600

    added domain models

---
+++ CRUDops/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace CRUDops.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool isAvailable { get; set; }
+
+        public int CategoryId { get; set; }
+
+
+    }
+}
